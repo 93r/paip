8e1b1d77da9cd5d2b357b49872eb4b0c1ed305b1
93r <ma_ko@riseup.net>
2019-01-04 17:56:20 +0100

    update gps-v2

--- ch4/gps-v2.cpp
+++ ch4/gps-v2.cpp
@@ -30,24 +30,12 @@ class gps {
 		/**
 		 * General Problem Solver: achieve all goals using ops
 		 */
-		gps(const symlist& state, const symlist& goals, const oplist& ops)
-			: state_(state), goals_(goals), ops_(ops) {
-				std::sort(state_.begin(), state_.end());
-				for (auto& op : ops_) {
-					std::sort(op.del_list.begin(), op.del_list.end());
-					std::sort(op.add_list.begin(), op.add_list.end());
-				}
-			}
-
+		gps(const symlist& state, const symlist& goals, const oplist& ops);
 		/**
 		 * The General Problem Solver starts working when called
 		 * like a Functor.
 		 */
-		bool operator()() {
-			auto pred = std::bind(&gps::achieve, this, _1);
-			return std::all_of(goals_.begin(), goals_.end(), pred);
-		}
-
+		bool operator()();
 		/**
 		 * Is intermediate information wanted? (setter)
 		 */
@@ -56,89 +44,30 @@ class gps {
 		/**
 		 * Try to achieve each goal, then make sure they hold.
 		 */
-		bool achieve_all(const symlist& goals) {
-			auto pred = std::bind(&gps::achieve, this, _1);
-			return std::all_of(goals.begin(), goals.end(), pred)
-				&&
-				subset_p(goals, state_);
-		}
+		bool achieve_all(const symlist& goals);
 		/**
 		 * A goal is achieved if it already holds, or if there is an
 		 * appropriate op for it that is applicable.
 		 */
-		bool achieve(const symbol& goal) {
-			//std::cout << "Checking " << goal << " membership\n";
-			if (member_p(goal, state_)) return true;
-			//std::cout << "Checking " << goal << " apply_op\n";
-			const auto& any_of_pred = std::bind(&gps::apply_op, this, _1);
-			const auto& copy_if_pred = std::bind(&gps::appropriate_p, this, goal, _1);
-			oplist tmp;
-
-			std::copy_if(ops_.begin(), ops_.end(), std::back_inserter(tmp), copy_if_pred);
-			for (auto& i : tmp) { std::cout << ">> " << i.action << "\n"; }
-			if (std::any_of(tmp.begin(), tmp.end(), any_of_pred)) return true;
-			return false;
-		}
+		bool achieve(const symbol& goal);
 		/**
 		 * Print a message and update state_ if op is applicable.
 		 */
-		bool apply_op(const op& op) {
-			const symlist& sl = op.preconds;
-			auto pred = std::bind(&gps::achieve, this, _1);
-
-			//std::cout << "op-action " << op.action;
-			if (std::all_of(sl.begin(), sl.end(), pred)) {
-				std::cout << " executing op-action " << op.action << "\n";
-
-				symlist tmp;
-				std::set_difference(
-						state_.begin(), state_.end(),
-						op.del_list.begin(), op.del_list.end(),
-						std::back_inserter(tmp)
-						);
-				state_ = tmp;
-				tmp.clear();
-				std::set_union(
-						state_.begin(), state_.end(),
-						op.add_list.begin(), op.add_list.end(),
-						std::back_inserter(tmp)
-					      );
-				state_ = tmp;
-				return true;
-			}
-			//std::cout << " fail!\n";
-
-			return false;
-		}
+		bool apply_op(const op& op);
 		/**
 		 * An op is appropriate to a goal if it is in its add list
 		 */
-		bool appropriate_p(const symbol& goal, const op& op) const {
-			return member_p(goal, op.add_list);
-		}
+		bool appropriate_p(const symbol& goal, const op& op) const;
 		/**
 		 * Is item a member of the sequence?
 		 */
-		bool member_p(const symbol& item, const symlist& seq) const {
-			return std::find(seq.begin(), seq.end(), item) != seq.end();
-		}
+		bool member_p(const symbol& item, const symlist& seq) const;
 		/**
 		 * Is one set (items) wholly contained in another (seq)?
 		 */
-		bool subset_p(const symlist& items, const symlist& seq) const {
-			auto pred = [&](const auto& i) {
-				return std::any_of(seq.begin(), seq.end(),
-						[&](const auto& j) {
-						return i == j;
-						});
-			};
-
-			return std::all_of(items.begin(), items.end(), pred);
-		}
+		bool subset_p(const symlist& items, const symlist& seq) const;
 	private: /* Auxiliary Functions */
-		void info(const std::string& msg) const {
-			if (want_ii_) { std::cerr << msg << "\n"; }
-		}
+		void info(const std::string& msg) const { if (want_ii_) { std::cout << msg << "\n"; } }
 	private: /* Special Variables */
 		/**
 		 * The current state: a list of conditions.
@@ -159,7 +88,90 @@ class gps {
 		bool want_ii_;
 };
 
-int main() {
+gps::gps(const symlist& state, const symlist& goals, const oplist& ops)
+	:
+		state_(state),
+		goals_(goals),
+		ops_(ops)
+{
+		std::sort(state_.begin(), state_.end());
+		for (auto& op : ops_) {
+			std::sort(op.del_list.begin(), op.del_list.end());
+			std::sort(op.add_list.begin(), op.add_list.end());
+		}
+}
+
+bool gps::operator()()
+{
+	auto pred = std::bind(&gps::achieve, this, _1);
+	return std::all_of(goals_.begin(), goals_.end(), pred);
+}
+
+bool gps::achieve_all(const symlist& goals)
+{
+	auto pred = std::bind(&gps::achieve, this, _1);
+	return std::all_of(goals.begin(), goals.end(), pred)
+		&&
+		subset_p(goals, state_);
+}
+
+bool gps::achieve(const symbol& goal)
+{
+	if (member_p(goal, state_)) return true;
+	const auto& any_of_pred = std::bind(&gps::apply_op, this, _1);
+	const auto& copy_if_pred = std::bind(&gps::appropriate_p, this, goal, _1);
+	oplist tmp;
+
+	std::copy_if(ops_.begin(), ops_.end(), std::back_inserter(tmp), copy_if_pred);
+	for (auto& i : tmp) { std::cout << ">> " << i.action << "\n"; }
+	if (std::any_of(tmp.begin(), tmp.end(), any_of_pred)) return true;
+	return false;
+}
+
+bool gps::apply_op(const op& op)
+{
+	const symlist& sl = op.preconds;
+	auto pred = std::bind(&gps::achieve, this, _1);
+
+	if (std::all_of(sl.begin(), sl.end(), pred)) {
+		std::cout << " executing op-action " << op.action << "\n";
+
+		symlist tmp;
+		std::set_difference(
+				state_.begin(), state_.end(),
+				op.del_list.begin(), op.del_list.end(),
+				std::back_inserter(tmp)
+				);
+		state_ = tmp;
+		tmp.clear();
+		std::set_union(
+				state_.begin(), state_.end(),
+				op.add_list.begin(), op.add_list.end(),
+				std::back_inserter(tmp)
+			      );
+		state_ = tmp;
+		return true;
+	}
+
+	return false;
+}
+
+bool gps::appropriate_p(const symbol& goal, const op& op) const { return member_p(goal, op.add_list); }
+bool gps::member_p(const symbol& item, const symlist& seq) const { return std::find(seq.begin(), seq.end(), item) != seq.end(); }
+bool gps::subset_p(const symlist& items, const symlist& seq) const
+{
+	auto pred = [&](const auto& i) {
+		return std::any_of(seq.begin(), seq.end(),
+				[&](const auto& j) {
+				return i == j;
+				});
+	};
+
+	return std::all_of(items.begin(), items.end(), pred);
+}
+
+int main()
+{
 	oplist school_ops {
 		{
 			"drive-son-to-school",
